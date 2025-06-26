a5f6aaa33e146b2f34c418f755a87ec298bfb87a
Boston008 <boston008@hotmail.com>
2025-06-26 20:29:48 +0800

    Update react-app.d.ts

--- web/src/types/react-app.d.ts
+++ web/src/types/react-app.d.ts
@@ -1,16 +1,9 @@
-<<<<<<< VITE_runtime_varables
+
 /// <reference types="react" />
 /// <reference types="react-dom" />
 
 declare namespace JSX {
   interface IntrinsicElements {
     [elemName: string]: any;
-=======
-import * as React from 'react';
-
-declare namespace JSX {
-  interface IntrinsicElements {
-    [elemName: string]: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
->>>>>>> main
   }
 }
